4c091b85f04cf8f77be3b5e1156c5b3c55b5d896
deepakdnayak <deepakdnayak2004@gmail.com>
2025-01-28 10:25:50 +0530

    Bug 1

--- app/(route)/test1/page.tsx
+++ app/(route)/test1/page.tsx
@@ -10,14 +10,19 @@ gsap.registerPlugin(ScrollTrigger);
 export default function Screen() {
     const leftScreenRef = useRef<HTMLDivElement>(null);
     const rightScreenRef = useRef<HTMLDivElement>(null);
+    const leftImageRef = useRef<HTMLDivElement>(null);
+    const rightImageRef = useRef<HTMLDivElement>(null);
     const stageRef = useRef<HTMLDivElement>(null);
 
     useEffect(()=> {
         const leftScreen = leftScreenRef.current;
         const rightScreen = rightScreenRef.current;
         const stage = stageRef.current;
+        const leftImage = leftImageRef.current;
+        const rightImage = rightImageRef.current;
+        const section = stageRef.current;
 
-        if(leftScreen && rightScreen){
+        if(leftScreen && rightScreen && leftImage && rightImage && section){
             gsap.timeline({
                 scrollTrigger: {
                     trigger: leftScreen,
@@ -51,6 +56,51 @@ export default function Screen() {
                 duration: 3,
             });
 
+
+            // Left Image Animation
+            gsap.timeline({
+                scrollTrigger: {
+                  trigger: leftImage,
+                  start: "top 60%",
+                  end: "+=300",
+                  scrub: true,
+                  markers: true,
+                  pin: true,
+                },
+            }).to(leftImage,{
+                x: () => {
+                  // Dynamically calculate the center position minus half the element width
+                  const vwCenter = section.offsetWidth / 2; // Center of the section
+                  const elementWidth = leftImage.offsetWidth; // Width of the left image container
+                  return vwCenter - elementWidth; // Center position accounting for element width
+                },
+                ease: "none",
+                duration: 4,
+              });
+
+             // Right Image Animation
+            gsap.timeline({
+                scrollTrigger: {
+                  trigger: rightImage,
+                  start: "top 60%",
+                  end: "+=300",
+                  scrub: true,
+                  markers: true,
+                  pin: true,
+                },
+            }).to(rightImage,{
+                x: () => {
+                  // Dynamically calculate the center position minus half the element width
+                  const vwCenter = section.offsetWidth / 2; // Center of the viewport
+                  const elementWidth = rightImage.offsetWidth; // Width of the right image container
+                  return -(vwCenter - elementWidth); // Center position accounting for element width
+                },
+                ease: "none",
+                duration: 4,
+            });
+
+
+            // Stage
             gsap.timeline({
                 scrollTrigger: {
                     trigger: stage,
@@ -61,16 +111,35 @@ export default function Screen() {
                     pin: true, 
                     pinSpacing: true,
                 },
-            }).to(rightScreen, {
+            }).to(stage, {
                 ease: "none",
-                duration: 3,
+                duration: 5,
             });
         }
     })
 
     return (
         <div className="relative">
-            <div ref={stageRef} className=" absolute top-0 left-0 bg-[url('/stage.jpg')] bg-center bg-cover bg-no-repeat h-screen w-full"></div>
+            <div ref={stageRef} className="absolute top-0 left-0 bg-[url('/stage.jpg')] bg-center bg-cover bg-no-repeat h-screen w-full">
+
+            {/* Left Image */}
+            <div
+                ref={leftImageRef}
+                className="absolute left-0 bottom-0 w-96 h-96 bg-[url('/womenLeft.png')] bg-center bg-cover bg-no-repeat flex items-start justify-end z-10"
+            >
+                <p className="text-white text-[100px] font-semibold">Aak</p>
+            </div>
+
+            {/* Right Image */}
+            <div
+                ref={rightImageRef}
+                className="absolute right-0 bottom-0 w-96 h-96 bg-[url('/womenRight.png')] bg-center bg-cover bg-no-repeat flex items-start justify-start z-10"
+            >
+                <p className="text-white text-[100px] font-semibold">riti</p>
+            </div>
+
+
+            </div>
             <div className="flex flex-row overflow-hidden">
                 
                 <div ref={leftScreenRef} className="w-1/2 h-screen bg-red-400 z-10">
